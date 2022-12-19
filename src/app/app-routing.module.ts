4f2f050842e274ddd370eb6149056eff21bce54e
Marina González <yelose85@gmail.com>
2022-12-19 20:27:01 +0100

    landing page completed, header with links functioning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EstadisticasComponent } from './views/estadisticas/estadisticas.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { JuegoComponent } from './views/juego/juego.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'juego', component: JuegoComponent },
+  { path: 'estadisticas', component: EstadisticasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { JuegoComponent } from './views/juego/juego.component';
+import { EstadisticasComponent } from './views/estadisticas/estadisticas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    JuegoComponent,
+    EstadisticasComponent
   ],
   imports: [
     BrowserModule,
