6f466dc983e20aec9fac039acfd6835e337ddad4
González Suárez <marina.gonzalez-suarez@capgemini.com>
2022-12-20 12:31:33 +0100

    view estadísticas front done

--- src/app/services/jugador.service.ts
+++ src/app/services/jugador.service.ts
@@ -9,26 +9,26 @@ export class JugadorService {
   public service: MainService;
 
   public async GetJugadores(): Promise<Jugador[]> {
-    return await this.service.HttpGet('/jugadores');
+    return await this.service.HttpGet('/estadisticas');
   }
 
   public async SearchJugadores(text: string): Promise<Jugador[]> {
     return await this.service.HttpGet(`/buscar/${encodeURIComponent(text)}`);
   }
   public async GetJugador(id: number): Promise<Jugador> {
-    return await this.service.HttpGet(`/jugadores/${id}`);
+    return await this.service.HttpGet(`/estadisticas/${id}`);
   }
 
   public async CreateJugador(jugador: Jugador): Promise<Jugador> {
-    return await this.service.HttpPost('/jugadores', jugador);
+    return await this.service.HttpPost('/estadisticas', jugador);
   }
 
   public async UpdateJugador(jugador: Jugador): Promise<Jugador> {
-    return await this.service.HttpPut(`/jugadores/${jugador.id}`, jugador);
+    return await this.service.HttpPut(`/estadisticas/${jugador.id}`, jugador);
   }
 
   public async DeleteJugador(id: number): Promise<{ message: string }> {
-    return await this.service.HttpDelete(`/jugadores/${id}`);
+    return await this.service.HttpDelete(`/estadisticas/${id}`);
   }
 
   constructor() {}
